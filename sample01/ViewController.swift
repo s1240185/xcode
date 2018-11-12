f97fab8f19a1792915eb8db2e2f43f849d6f6132
加納健汰 <pb@wlan-napt-002.u-aizu.ac.jp>
2018-11-12 12:24:51 +0900

    10回押すたびに「やったね」が出るやつ

--- sample01/ViewController.swift
+++ sample01/ViewController.swift
@@ -10,15 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet var label: UILabel!
-
+    @IBOutlet var labeltest :UILabel!
+    @IBOutlet var buttonTest :UIButton!
+    
+    var count = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        label.text = "Hello World"
-
+        
+        labeltest.text = "Swift Test"
+        
+        buttonTest.setTitle("Button",for:UIControl.State.normal)
+        
     }
-
+    
+    @IBAction func buttonTapped(_ sender : Any) {
+        count += 1
+        
+        labeltest.text = String(count)
+        
+        if(count%10 == 0){
+            labeltest.text = "やったね"
+        }
+        
+        /*
+        if(count%2 == 0){
+            labeltest.text = "Swift Test"
+        }
+        else{
+            labeltest?.text = "tapped !"
+        }
+ */
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
 
 }
 
